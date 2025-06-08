f02025486ac8cedec2e76b037cf9c989b99a1206
abelle18 <abelle.marie18@gmail.com>
2025-06-08 01:45:50 +0000

    last commit

---
+++ HelloWorld.java
@@ -0,0 +1,19 @@
+//# Hello-World
+//Lab 1 Hello, World!
+
+public class HelloWorld
+{
+    public static void main(String args[])
+    {
+        System.out.println("Hello World"); // This is printing an output called Hello World.
+        System.out.println("Annabelle Garcia"); // This is printing an output of my name.
+        System.out.println("Sushi"); // This is printing an output of my favorite food.
+        System.out.println("Ariana Grande"); // This is printing an output of a celebrity I would like to meet.
+        System.out.println("Dishes"); // This is printing an output of my least favorite chore.
+        System.out.println("Playing Minecraft"); // This is printing an output of what I would rather be doing right now, other than completing this lab assignment.
+
+        // The main() method tells the JVM where to start running your code.
+        // The system.out.print.ln() is used to display output because whatever you write is going to be displayed on the console.
+        // The purpose of comments in the program are used to organize, explain and allow the code to become more user friendly to others reading the code.
+    }
+}
